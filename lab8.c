e1a2f3e0dd0c899e51818c187bf6724e42dee05c
6804062612323-Thanapat <s6804062612323@email.kmutnb.ac.th>
2026-02-23 22:58:37 +0700

    Update lab8.c

--- lab8.c
+++ lab8.c
@@ -1,16 +1,29 @@
 #include <stdio.h>
 
 int findMax(int num[]);
+void printAr(int myAr[],int n);
+void addone(int number[], int add[], int sizeNum);
+void addtwo(int number[], int pos[], int x, int sizeNum, int sizePos);
+void addthree(int number[], int pos[][2], int size);
+
 int main() {
 	int number[5] = {20,50,100,99,9};
+	int numberSize = sizeof(number)/sizeof(number[0]);
+	int add[5] = {2,4,6,8,10};
+	int pos[] = {1,3};
+	int posSize = sizeof(pos)/sizeof(pos[0]);
+	int pos2d1[][2] = {{0,3},{2,4}};
+	int pos2d2 = sizeof(pos2d1)/sizeof(pos2d2);
 	int max;
 
 	max = findMax(number);
-	printf("number[1] : %d\n",number[0]);
-	printf("number[2] : %d\n",number[1]);
-	printf("number[3] : %d\n",number[2]);
-	printf("number[4] : %d\n",number[3]);
-	printf("number[5] : %d\n",number[4]);
+	printAr(number,numberSize);
+	addone(number,add,numberSize);
+	printf("\n");
+	addtwo(number,pos,5,numberSize,posSize);
+	printf("\n");
+	addthree(number,pos2d1,pos2d2);
+	printf("\n");
 	printf("Maximum of these number is %d\n",max);
 }
 
@@ -24,12 +37,34 @@ int findMax(int num[]) {
 	}
 	return maximum;
 }
+void printAr(int myAr[],int n){
+    for(int i=0;i<n;i++){
+        printf("number[%d] : %d\n",i,myAr[i]);
+    }
+}
 
-/* result
-number[1] : 20
-number[2] : 50
-number[3] : 100
-number[4] : 99
-number[5] : 9
-Maximum of these number is 100
-*/
+void addone(int number[], int add[], int sizeNum){
+    for(int i=0;i<sizeNum;i++){
+        number[i]+=add[i];
+        printf("%d ",number[i]);
+    }
+}
+
+void addtwo(int number[], int pos[], int x, int sizeNum, int sizePos){
+    for(int i=0;i<sizePos;i++){
+        number[pos[i]]+=x;
+        printf("%d ",number[i]);
+    }
+}
+
+void addthree(int number[], int pos[][2], int size){
+    for (int i = 0; i < size; i++) {
+        int p1 = pos[i][0];
+        int p2 = pos[i][1];
+
+        if (p1 >= 1 && p1 <= size) {
+            printf("Add %d to number[%d]\n", p2, p1);
+            number[p1 - 1] += p2;
+        }
+    }
+}
